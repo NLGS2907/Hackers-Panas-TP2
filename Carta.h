084636e9d2a8a5a7d823ec6b0e09d92ef64ed4b6
Guillermina Hoff <72233846+guillehoff@users.noreply.github.com>
2021-06-14 21:02:44 -0300

    Subo Carta.h (template) contiene definiciones

---
+++ Carta.h
@@ -0,0 +1,56 @@
+#ifndef CARTA_H_
+#define CARTA_H_
+
+#include<iostream>
+#include<string>
+
+template <class T>
+class Carta {
+
+private:
+
+	T descripcion;
+
+public:
+
+	/*
+	 * PRE:
+	 * POST: asigna decripcion
+	 */
+	Carta(T descripcion);
+
+	/*
+	 * PRE:
+	 * POST: asigna decripcion
+	 */
+	void asignarDescripcion(T descripcion);
+
+	/*
+	 * PRE:
+	 * POST: devuelve descripcion de carta
+	 */
+	T obtenerDescripcion();
+
+};
+
+
+/* ----- DEFINICIONES ----- */
+template <class T>
+Carta<T>::Carta(T descripcion)  {
+
+	this->descripcion = descripcion;
+}
+
+template <class T>
+void Carta<T>::asignarDescripcion(T descripcion) {
+
+	this->descripcion = descripcion;
+}
+
+template <class T>
+T Carta<T>::obtenerDescripcion() {
+
+	return descripcion;
+}
+
+#endif /* CARTA_H_ */
