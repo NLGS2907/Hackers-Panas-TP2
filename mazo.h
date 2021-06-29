ddf99929f33c2d7b6206ae98b5dc56d4c5d43f39
Guillermina Hoffmann <72233846+guillehoff@users.noreply.github.com>
2021-06-29 00:20:22 -0300

    Agregue constructor sin parametro

--- mazo.h
+++ mazo.h
@@ -12,12 +12,40 @@ private:
 	int cantidadDeCartas;
 
 public:
-	Mazo(int);
+	/*
+	*pre:
+	*post:
+	*/
+	Mazo();
 
+	/*
+	*pre:
+	*post:
+	*/
+	Mazo(int numeroDeCartas);
+
+	/*
+	*pre:
+	*post:
+	*/
 	~Mazo();
 
+	/*
+	*pre:
+	*post:
+	*/
+	void setCantidadDeCartas(int numeroDeCartas);
+
+	/*
+	*pre:
+	*post:
+	*/
 	Carta* acolarCarta(Carta* carta);
 
+	/*
+	*pre:
+	*post:
+	*/
 	Carta* darCarta();
 
 
@@ -25,28 +53,35 @@ public:
 
 /* ----- DEFINICIONES ----- */
 
+Mazo::Mazo() {
+	mazo = new Cola<Carta*>;
+	cantidadDeCartas = 20; //puse un numero cualquiera
+}
 
-Mazo::Mazo(int numeroDeCartas){
-	if(numeroDeCartas > 0){
+Mazo::Mazo(int numeroDeCartas) {
+	if(numeroDeCartas > 0) {
 		cantidadDeCartas = numeroDeCartas;
 	}
 	mazo = new Cola<Carta*>;
 }
 
+void Mazo::setCantidadDeCartas(int numeroDeCartas) {
+	cantidadDeCartas = numeroDeCartas;
+}
 
-Carta* Mazo::acolarCarta(Carta* carta){
+Carta* Mazo::acolarCarta(Carta* carta) {
 
-		mazo->acolar(carta);
+	mazo->acolar(carta);
 
 	return carta;
 }
 
-Carta* Mazo::darCarta(){ //desacolar ya checkea q no este vacia
+Carta* Mazo::darCarta() { //desacolar ya checkea q no este vacia
 
 	return mazo->desacolar();
 }
 
-Mazo::~Mazo(){
+Mazo::~Mazo() {
 	delete mazo;
 }
 
